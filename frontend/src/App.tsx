bf72abd6bee1c65da544113577f6f2516a9abfbb
MM <143347704+MacielMaximiliano@users.noreply.github.com>
2026-05-25 15:54:01 -0300

    componente para testing de front chat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import AppLayout from './layouts/AppLayout'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Diary from './pages/Diary'
+import ChatTest from './pages/ChatTest'
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/chat-test" element={<ChatTest />} />
       <Route element={<AppLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/diary" element={<Diary />} />
