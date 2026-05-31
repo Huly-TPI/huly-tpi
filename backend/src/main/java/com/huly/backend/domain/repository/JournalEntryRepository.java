9bda42d4c3a2d6e6eb77b587b586744816ad45f5
santicrescenzo <72558776+santicrescenzo@users.noreply.github.com>
2026-05-31 17:45:41 -0300

    endpoint get journal

--- backend/src/main/java/com/huly/backend/domain/repository/JournalEntryRepository.java
+++ backend/src/main/java/com/huly/backend/domain/repository/JournalEntryRepository.java
@@ -3,6 +3,9 @@ package com.huly.backend.domain.repository;
 import com.huly.backend.domain.model.JournalEntry;
 import com.huly.backend.domain.model.enums.Mood;
 
+import java.util.List;
+
 public interface JournalEntryRepository {
     JournalEntry save(Long userId, String content, Mood mood);
+    List<JournalEntry> findAllByUserId(Long userId);
 }
