23a73291f93700c8fd17f10910df520827797de5
Avril Sandoval <avril.sandoval.004@gmail.com>
2026-06-01 17:08:54 -0300

    SCRUM-194: refactor CompleteUserGoalUseCase to include transaction management , update UserGoalRepoImpl to set goal ID during entity creation

--- backend/src/main/java/com/huly/backend/domain/useCase/userGoal/CompleteUserGoalUseCase.java
+++ backend/src/main/java/com/huly/backend/domain/useCase/userGoal/CompleteUserGoalUseCase.java
@@ -4,8 +4,10 @@ import com.huly.backend.domain.model.UserGoal;
 import com.huly.backend.domain.model.enums.GoalStatus;
 import com.huly.backend.domain.repository.UserGoalRepository;
 import com.huly.backend.exception.NotFoundException;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +15,13 @@ public class CompleteUserGoalUseCase {
 
     private final UserGoalRepository userGoalRepository;
 
+    @Transactional
     public UserGoal execute(Long id) {
         UserGoal goal = userGoalRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("UserGoal", "id", id));
+        if (goal.getStatus() == GoalStatus.COMPLETED) {
+            return goal;
+        }
         goal.setStatus(GoalStatus.COMPLETED);
         return userGoalRepository.save(goal);
     }
