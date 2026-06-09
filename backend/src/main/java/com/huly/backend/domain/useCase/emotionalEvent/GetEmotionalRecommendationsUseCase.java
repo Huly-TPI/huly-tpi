036e9fc0408aa57b67af3f70003d3d3826d2dff4
MM <143347704+MacielMaximiliano@users.noreply.github.com>
2026-06-09 11:58:43 -0300

    rf ⚙remove service and create beans

--- backend/src/main/java/com/huly/backend/domain/useCase/emotionalEvent/GetEmotionalRecommendationsUseCase.java
+++ backend/src/main/java/com/huly/backend/domain/useCase/emotionalEvent/GetEmotionalRecommendationsUseCase.java
@@ -7,11 +7,9 @@ import com.huly.backend.domain.model.EmotionalRecommendationResult;
 import com.huly.backend.domain.repository.ActivityRepository;
 import com.huly.backend.domain.service.EmotionalRecommendationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 @RequiredArgsConstructor
 public class GetEmotionalRecommendationsUseCase {
 
--- backend/src/main/java/com/huly/backend/domain/useCase/userGoal/CompleteUserGoalUseCase.java
+++ backend/src/main/java/com/huly/backend/domain/useCase/userGoal/CompleteUserGoalUseCase.java
@@ -7,10 +7,8 @@ import com.huly.backend.domain.repository.UserGoalRepository;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @RequiredArgsConstructor
 public class CompleteUserGoalUseCase {
 
